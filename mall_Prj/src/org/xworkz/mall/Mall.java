627fd24292135c2ee89575cd906b1d5a3877d62a
rathi79 <60299865+rathi79@users.noreply.github.com>
2020-01-25 11:18:49 -0800

    Mall Project
    simple Mall Project

---
+++ mall_Prj/src/org/xworkz/mall/Mall.java
@@ -0,0 +1,8 @@
+package org.xworkz.mall;
+
+public class Mall {
+	public void windowshopping() {
+		System.out.println("Window shopping was very good");
+	}
+
+}
---
+++ mall_Prj/src/org/xworkz/mall/TesterMall.java
@@ -0,0 +1,11 @@
+package org.xworkz.mall;
+
+public class TesterMall {
+
+	public static void main(String[] args) {
+		Mall mall=new Mall();
+		mall.windowshopping();
+
+	}
+
+}
